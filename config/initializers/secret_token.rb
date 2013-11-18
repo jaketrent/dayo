2bfefddfd5e0b096b83e292550ddb5433104f964
jaketrent <trent.jake@gmail.com>
2013-11-18 09:13:55 -0700

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dayo::Application.config.secret_key_base = 'eeb92d6db848164ebc5ddd603f43e1e322201426f779944f8ff39088742185419b2ca7cdff395c216813a084e8cc979a6cc009bae197787b79110d602205e3df'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dayo::Application.config.session_store :cookie_store, key: '_dayo_session'
