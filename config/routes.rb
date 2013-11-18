60747baf1df91adb8909d87b0a3fc460b00c69b1
jaketrent <trent.jake@gmail.com>
2013-11-18 11:12:05 -0700

    add route url

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Dayo::Application.routes.draw do
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
   devise_for :users
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -18,7 +18,7 @@ Dayo::Application.routes.draw do
   #   resources :products
   get '/accident/:name', to: 'accident#show'
   resources :accident
-
+  root "accident#index"
 
   # Example resource route with options:
   #   resources :products do
